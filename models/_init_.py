d0e08addf3f93d35eb7c01d190389bce76c4b914
CodeLordGh <babohkossi228@icloud.com>
2023-07-17 14:49:44 +0000

    creation and completion of the task

--- models/_init_.py
+++ models/_init_.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
 storage.reload()
-
